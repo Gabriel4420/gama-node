56c2e4edaf980646938b860c8477359985d0a9b1
= <gabriel_rodrigues_perez@hotmail.com>
2020-07-11 01:17:32 -0300

    Crud quase completo

--- 06-cli/database.js
+++ 06-cli/database.js
@@ -4,24 +4,30 @@ const { promisify } = require('util');
 const readFileAsync = promisify(readFile);
 const writeFileAsync = promisify(writeFile);
 class Database{
+
   constructor(){
     this.NOME_ARQUIVO = 'herois.json'
   }
+
   async obterDadosArquivo(){
     const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8');
     return JSON.parse(arquivo.toString());
   }
+
   async escreverArquivo(dados){
     await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados));
     return true;
   }
+
   async cadastrar(heroi) {
     const dados = await this.obterDadosArquivo();
     //workaround para simular um id
     const id = heroi.id <= 2 ? heroi.id : Date.now();
     const heroiComId = {
+            
       ...heroi,
       id,
+      
     };
 
     return await this.escreverArquivo([...dados, heroiComId]);
--- 06-cli/index.js
+++ 06-cli/index.js
@@ -7,22 +7,44 @@ async function main() {
   Commander.version('v1')
   .option('-n, --nome [value]',"Nome do Herói")
   .option('-p, --poder [value]',"Poder do Herói")
+  .option('-i, --id [value]',"Id do Herói")
+
   .option('-c, --cadastrar',"Cadastrar um Herói")
-  .parse(process.argv)
+  .option('-l, --listar',"Listar um Herói")
+  .option('-r, --remove [value]',"Deleta um Herói pelo id")
+  .parse(process.argv);
   const heroi = new Heroi(Commander)
   try {
     if(Commander.cadastrar){
-      
-      const resultado = await Database.cadastrar()
+     
+      const resultado = await Database.cadastrar(heroi)
 
-      if(resultado == false){
+      if(!resultado){
         console.error('Heroi não foi cadastrado');
         return;
       }
       console.log('Heroi cadastrado com sucesso');
     }
+
+    if(Commander.listar){
+      const resultado = await Database.listar();
+
+      if(resultado.length <= 0)
+        console.error('Heroi não encontrado');
+      else
+        console.log('Heroi encontrado com sucesso' , resultado);
+    }
+
+    if(Commander.remove){
+      const resultado = await Database.remover(heroi.id);
+
+      if(!resultado)
+        console.error('Não foi possivel remover o heroi')
+      else  
+        console.log('Heroi removido com sucesso')
+    }
   } catch (error) {
-      console.error('Deu Ruim')
+      console.error('Deu Ruim', error);
   }
 }
 
